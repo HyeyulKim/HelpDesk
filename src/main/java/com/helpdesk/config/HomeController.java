1fe39d68860ecb99f8fa29bf09293a496caee0d5
cyn1025@naver.com <cyn1025@naver.com>
2026-09-16 16:08:42 +0900

    refactor: 홈 진입 라우팅 정리 및 미사용 index.html 제거
    - HomeController: 로그인 사용자는 /requests로, 비로그인 방문자는 /user/login으로 리다이렉트
    
    - 더 이상 렌더링되지 않는 templates/index.html 삭제

--- src/main/java/com/helpdesk/config/HomeController.java
+++ src/main/java/com/helpdesk/config/HomeController.java
@@ -1,5 +1,7 @@
 package com.helpdesk.config;
 
+import com.helpdesk.security.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String home(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        // 로그인 사용자는 문의함으로, 비로그인 방문자는 로그인 화면으로 바로 이동
+        if (userDetails != null) {
+            return "redirect:/requests";
+        }
+        return "redirect:/user/login";
     }
 }
